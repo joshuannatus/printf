647adfc95a4057e1da83a9dd74bdcea260d0d923
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-03-25 16:37:20 -0700

    testing

--- _printf.c
+++ _printf.c
@@ -1,48 +1,63 @@
 #include "main.h"
 
+/**
+ * _printf - produces output.
+ * @format: character string.
+ *
+ * Return: the number of characters printed.
+ */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
+	va_list args;
+	int count = 0;
+	va_start(args, format);
 
-    int count = 0;
-    va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c' :
+					{
+						char c = (char)va_arg(args, int);
+						putchar(c);
+						count++;
+						break;
+					}
+				case 's':
+					{
+						char *s = va_arg(args, char*);
+						while (*s)
+						{
+							putchar(*s);
+							s++;
+							count++;
+						}
+						break;
+					}
+				case '%':
+					{
+						putchar('%');
+						count++;
+						break;
+					}
+				default:
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
 
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            switch (*format) {
-		 case 'c' : {
-                    char c = (char)va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    char *s = va_arg(args, char*);
-                    while (*s) {
-                        putchar(*s);
-                        s++;
-                        count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+	va_end(args);
 
-    va_end(args);
-
-    return (count);
+	return (count);
 }
 
